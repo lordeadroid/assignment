693d2b3f7579ba38197c1d5d5493b91d12eeb0b7
Rishabh <lordeadroid@users.noreply.github.com>
2023-04-12 13:07:43 +0530

    shape test case passed

--- test/shape-test.js
+++ test/shape-test.js
@@ -1,7 +1,8 @@
-const test = require("../src/square.js");
+const shape = require("../src/square.js");
+const test = require("../lib/testing.js");
 
 const testSquare = function() {
-  test.assertEquality("****\n****\n****\n****\n", square(4), 'should give square of given length');
+  test.assertEquality("****\n****\n****\n****\n", shape.square(4), 'should give square of given length');
 }
 
 testSquare();
